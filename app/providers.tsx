10df5a3f16c01e15d5b463c8755ddc84727fecf7
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-27 23:25:45 +0200

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { createBrowserClient } from "@supabase/ssr";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionContextProvider supabaseClient={supabase}>
+      {children}
+    </SessionContextProvider>
+  );
+}
