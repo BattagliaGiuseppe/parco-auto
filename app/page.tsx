cbe647926fbf90babbee73cd8e90f7ac5211d768
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-27 23:20:51 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
 
 export default function Home() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="flex-1 bg-gray-100 min-h-screen p-6">
-        <Dashboard />
-      </main>
-    </div>
+    <main className="flex-1 bg-gray-100 min-h-screen p-6">
+      <Dashboard />
+    </main>
   );
 }
