a07e1e77bdff764de358e239cdfe081eda40ff81
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-28 15:20:08 +0200

    Delete app/calendario directory

--- app/calendario/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CalendarioPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">📅 Calendario</h1>
-      <p>Qui verrà mostrato il calendario eventi.</p>
-    </div>
-  );
-}
