20dba047f8e12175d803c5b75d1a6bb80a801d08
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-28 15:09:53 +0200

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,13 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+    },
+  }
 );
 
 export default function Providers({ children }: { children: React.ReactNode }) {
