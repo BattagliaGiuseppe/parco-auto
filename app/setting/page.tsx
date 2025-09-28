24876eabd742fa0a942216d3ca30a1dfe3eb2073
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-09-28 15:18:56 +0200

    Create page.tsx

---
+++ app/setting/page.tsx
@@ -0,0 +1,8 @@
+export default function ImpostazioniPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">⚙️ Impostazioni</h1>
+      <p>Qui potrai gestire le impostazioni del sistema.</p>
+    </div>
+  );
+}
