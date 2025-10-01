f8d58b64f782ffdbf88476ac872a61b3e144bfb0
BattagliaGiuseppe <pbattagliap@gmail.com>
2025-10-01 11:57:13 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          DEFAULT: "#FFD700",   // oro base
+          dark: "#B8860B",      // oro scuro (hover)
+          light: "#FFECB3",     // oro chiaro (accento)
+        },
+      },
+    },
   },
-  darkMode: "class",
   plugins: [],
 };
